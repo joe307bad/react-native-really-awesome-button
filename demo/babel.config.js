73d8922b284b6be9fd5cb9feb91f7c2631f12ea6
Joe Badaczewski <jbadaczewski@softwriters.com>
2021-10-08 11:15:25 -0400

    last day

--- demo/babel.config.js
+++ demo/babel.config.js
@@ -1,6 +1,34 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function bpe(api) {
+  //   api.cache(true);
+  const presets = [
+    'babel-preset-expo',
+    'module:metro-react-native-babel-preset',
+    'module:react-native-dotenv'
+  ];
+
+  const moduleResolver = [
+    'module-resolver',
+    {
+      root: './',
+      alias: {
+        i18n: './i18n'
+      }
+    }
+  ];
+
+  const plugins = [moduleResolver];
+
+  const envDevelopment = {
+    presets: presets,
+    plugins: ['@babel/transform-react-jsx-source', moduleResolver]
+  };
+
+  if (api.env(['development', 'test'])) {
+    return envDevelopment;
+  }
+
   return {
-    presets: ["babel-preset-expo"]
+    presets: presets,
+    plugins: plugins
   };
 };
